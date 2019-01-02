467703cc7ec432ce0cf3cd6acb7c0e3fa9f9649b
yossieven <yossi.even@gmail.com>
2019-01-03 00:23:04 +0200

    .

--- db/migrations/20190102231450_create_product.js
+++ db/migrations/20190102231450_create_product.js
@@ -9,5 +9,5 @@ exports.up = function (knex, Promise) {
 };
 
 exports.down = function (knex, Promise) {
-    return knex.schema.dropTable('product')
+    return knex.schema.dropTable('product');
 };
--- db/migrations/20190103001816_create_cart.js
+++ db/migrations/20190103001816_create_cart.js
@@ -1,9 +1,11 @@
 exports.up = function (knex, Promise) {
-    table.increments();
-    table.integer('user_id').references('id').inTable('user');
-    table.timestamp('creation_date').defaultTo(knex.fn.now());
+    return knex.schema.createTable('cart', function (table) {
+        table.increments();
+        table.integer('user_id').references('id').inTable('user');
+        table.timestamp('creation_date').defaultTo(knex.fn.now());
+    });
 };
 
 exports.down = function (knex, Promise) {
-
+    return knex.schema.dropTable('cart');
 };
